99bab18312e3b591f86b9a0667175a6c8ab4df2d
ejazahmed195 <144973579+ejazahmed195@users.noreply.github.com>
2025-06-30 11:11:03 +0500

    Update class.verify-purchase.php

--- class.verify-purchase.php
+++ class.verify-purchase.php
@@ -3,7 +3,7 @@ class EnvatoApi2 {
     // Bearer, no need for OAUTH token, change this to your bearer string
     // https://build.envato.com/api/#token
     
-    private static $bearer = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"; // replace the API key here.
+    private static $bearer = "aOIC3DTYvN1Y8IK2MtJ3oIp20LhB73YM"; // replace the API key here.
     
     static function getPurchaseData( $code ) {
       
